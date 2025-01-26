5b9dd9af2ed2aca5668833bb132fe03eaa7fd17a
Vitor Diniz <dinizadroit@gmail.com>
2025-01-26 02:42:30 -0300

    tailwind setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import './index.css';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from './App';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="bg-red-500">Hello World!</div>
   </StrictMode>,
 );
